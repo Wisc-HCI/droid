68dcfa0ddc10142f9d3ec52f4fb9c00dc2d8f6dd
Callie Kim <cykim6@wisc.edu>
2026-06-10 16:19:00 -0500

    Update Docker setup to mount entrypoint script and fix conda activation

--- .docker/laptop/entrypoint.sh
+++ .docker/laptop/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # activate conda
-source ~/miniconda3/bin/activate
+# NOTE: use conda.sh, not `source ~/miniconda3/bin/activate`. The latter inherits
+# this script's positional args ($@ = the user command) and feeds them to the
+# legacy activate script, causing "activate does not accept more than one argument".
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate robot
 
 # run user command
